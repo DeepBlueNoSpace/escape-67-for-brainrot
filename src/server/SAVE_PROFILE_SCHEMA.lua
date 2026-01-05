8b94ee1237538a63c4914c45ca5f5baaf9801c60
Nicolas Markham <nicolasjmarkham@gmail.com>
2026-01-05 17:49:39 +0000

    d

---
+++ src/server/SAVE_PROFILE_SCHEMA.lua
@@ -0,0 +1,9 @@
+local DataStoreService = game:GetService("DataStoreService")
+
+local DATASCHEMA = {}
+
+
+DATASCHEMA.RobuxSpent = 0
+
+-- Owned by WeightService
+return DATASCHEMA
---
+++ src/shared/TEST_CONFIGS.lua
@@ -0,0 +1,24 @@
+local TEST_CONFIGS = {}
+TEST_CONFIGS.TESTING = true
+
+--configurables
+
+TEST_CONFIGS.SOUNDTRACK_OFF = true
+TEST_CONFIGS.QUICK_PAIR = true
+TEST_CONFIGS.DISABLE_LOADSCREEN = true
+TEST_CONFIGS.TEST_STAGE = nil
+
+local PLACEID = 138314303849362
+local STAGINGID = 0 --112899678167402 SUCK MY BALLS TEST CONFIGS SHOULD BE ON IN STAGING TURN THEM OFF IF U DONT WANT EM
+
+function TEST_CONFIGS.Is(value)	
+	return TEST_CONFIGS.TESTING and TEST_CONFIGS[value]
+end
+
+local RunService = game:GetService("RunService")
+
+if not RunService:IsStudio() then--) and (game.PlaceId == PLACEID or game.PlaceId == STAGINGID) then
+TEST_CONFIGS.TESTING = false
+end
+
+return TEST_CONFIGS
